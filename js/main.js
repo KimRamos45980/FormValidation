5be2e2208712bfde8a1629c63f98187dae6da5f1
Kimberly Ramos <hinagikukatsura45980@gmail.com>
2022-04-19 17:20:38 -0700

    Created function to test if textbox has data

--- js/main.js
+++ js/main.js
@@ -3,5 +3,16 @@ window.onload = function () {
     formBtn.onclick = main;
 };
 function main() {
-    alert("Reg button was clicked");
+    isTextPresent("first-name", "First name is required");
+    isTextPresent("last-name", "Last name is required");
+}
+function isTextPresent(id, errMsg) {
+    var txtbox = document.getElementById(id);
+    var txtBoxValue = txtbox.value;
+    if (txtBoxValue == "") {
+        var errSpan = txtbox.nextElementSibling;
+        errSpan.innerText = errMsg;
+        return false;
+    }
+    return true;
 }
